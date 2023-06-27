7ae5e8a3d30f8b33918b466b75a7be9f657de812
TiricNguyen\CongTien <nguyentiric@gmail.com>
2023-06-27 14:49:43 +0700

    controller thymleaf

--- src/main/java/com/example/template_project/service/NhanVienService.java
+++ src/main/java/com/example/template_project/service/NhanVienService.java
@@ -3,17 +3,19 @@ package com.example.template_project.service;
 import com.example.template_project.model.request.CreateNhanVienRequest;
 import com.example.template_project.model.request.UpdateNhanVienRequest;
 import com.example.template_project.model.response.NhanVienResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public interface NhanVienService {
 
-    List<NhanVienResponse> getAll();
+    Page<NhanVienResponse> pageNhanVien(Integer pageNo, Integer size);
 
     NhanVienResponse create(CreateNhanVienRequest createNhanVienRequest);
 
-    NhanVienResponse update(UpdateNhanVienRequest updateNhanVienRequest,Integer id);
+    NhanVienResponse update(UpdateNhanVienRequest updateNhanVienRequest, Integer id);
 
     NhanVienResponse getOne(Integer id);
 
--- src/main/java/com/example/template_project/service/impl/NhanVienServiceImpl.java
+++ src/main/java/com/example/template_project/service/impl/NhanVienServiceImpl.java
@@ -1,10 +1,6 @@
 package com.example.template_project.service.impl;
 
-import com.example.template_project.entity.ChucVu;
 import com.example.template_project.entity.NhanVien;
-import com.example.template_project.exception.DuplicateRecordException;
-import com.example.template_project.exception.InternalServerException;
-import com.example.template_project.exception.NotFoundException;
 import com.example.template_project.model.mapper.NhanVienMapper;
 import com.example.template_project.model.request.CreateNhanVienRequest;
 import com.example.template_project.model.request.UpdateNhanVienRequest;
@@ -13,10 +9,11 @@ import com.example.template_project.repository.ChucVuRepository;
 import com.example.template_project.repository.NhanVienRepository;
 import com.example.template_project.service.NhanVienService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -32,44 +29,23 @@ public class NhanVienServiceImpl implements NhanVienService {
     ChucVuRepository chucVuRepository;
 
     @Override
-    public List<NhanVienResponse> getAll() {
-        return nhanVienMapper.listNhanVienEntityToNhanVienRequest(nhanVienRepository.findAll());
+    public Page<NhanVienResponse> pageNhanVien(Integer pageNo, Integer size) {
+        Pageable pageable = PageRequest.of(pageNo, size);
+        Page<NhanVien> nhanVienPage = nhanVienRepository.findAll(pageable);
+        return nhanVienPage.map(nhanVienMapper::nhanVienEntityToNhanVienResponse);
     }
 
     @Override
     public NhanVienResponse create(CreateNhanVienRequest createNhanVienRequest) {
-        try {
             NhanVien nhanVien = nhanVienMapper.createNhanVienRequestToNhanVienEntity(createNhanVienRequest);
             return nhanVienMapper.nhanVienEntityToNhanVienResponse(nhanVienRepository.save(nhanVien));
-        } catch (DataIntegrityViolationException ex) {
-            throw new DuplicateRecordException("Failed to create product. Possibly duplicate record." + ex);
-        } catch (Exception ex) {
-            throw new InternalServerException("Failed to create product due to an unexpected error." + ex);
-        }
     }
 
     @Override
     public NhanVienResponse update(UpdateNhanVienRequest updateNhanVienRequest, Integer id) {
 
         Optional<NhanVien> nhanVienById = nhanVienRepository.findById(id);
-
-        if (!nhanVienById.isPresent()) {
-            throw new NotFoundException("NhanVien not found with id " + id);
-        }
-
         NhanVien nhanVien = nhanVienById.get();
-        ChucVu chucVu = nhanVien.getChucVu();
-
-        if (chucVu == null || chucVu.getId() == null) {
-            throw new NotFoundException("ChucVu not found with id " + id);
-        }
-
-        Optional<ChucVu> chucVuById = chucVuRepository.findById(chucVu.getId());
-
-        if (!chucVuById.isPresent()) {
-            throw new NotFoundException("ChucVu not found with id " + chucVu.getId());
-        }
-
         nhanVien = nhanVienMapper.updateNhanVienRequestToNhanVienEntity(updateNhanVienRequest);
         return nhanVienMapper.nhanVienEntityToNhanVienResponse(nhanVienRepository.save(nhanVien));
     }
@@ -77,9 +53,6 @@ public class NhanVienServiceImpl implements NhanVienService {
     @Override
     public NhanVienResponse getOne(Integer id) {
         Optional<NhanVien> nhanVienById = nhanVienRepository.findById(id);
-        if (!nhanVienById.isPresent()) {
-            throw new NotFoundException("NhanVien not found with id " + nhanVienById);
-        }
         return nhanVienMapper.nhanVienEntityToNhanVienResponse(nhanVienById.get());
     }
 
